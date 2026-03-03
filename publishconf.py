e622d7481e2ba08a99e5b252a07311e3b15b013b
cngene <ngenech@gmail.com>
2026-03-03 10:09:10 -0600

    Remove social tab

--- publishconf.py
+++ publishconf.py
@@ -11,8 +11,8 @@ from pelicanconf import *
 SITEURL = "https://chika-ngene.github.io/Comp2600-Assignment2"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+# FEED_ALL_ATOM = "feeds/all.atom.xml"
+# CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
